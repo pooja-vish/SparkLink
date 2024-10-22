c5e472a0ba6f62a184725ebf1ee4e3dd40880ece
joshua-daniel <cherrydaniel11@gmail.com>
2024-10-22 12:18:49 -0400

    Revert "Merge pull request #23 from pooja-vish/issue-5"
    This reverts commit 618aa1f960f70d09c154e93d66bcad8988f7f4ed, reversing
    changes made to 8d60ef85596305e3beb038149cdfa3c41db20fba.

--- sparklink/src/component/createproject/CreateProjectComponent.jsx
+++ sparklink/src/component/createproject/CreateProjectComponent.jsx
@@ -2,24 +2,16 @@ import React, { useState, useRef } from "react";
 import "./CreateProjectComponent.css";
 import MenuComponent from "../../component/menu/MenuComponent";
 import MasterComponent from "../MasterComponent";
+import DatePicker from "react-datepicker";
 
 const CreateProjectComponent = () => {
-  const dateInputRef = useRef(null);
-  const [isOtherPurposeChecked, setIsOtherPurposeChecked] = useState(false);
-  const [isOtherTypeChecked, setIsOtherTypeChecked] = useState(false);
+    const dateInputRef = useRef(null);
 
-  const triggerDatePicker = () => {
-    if (dateInputRef.current) {
-      dateInputRef.current.showPicker();
-    }
-  };
-
-  const handlePurposeCheckBoxChange = (e) => {
-    setIsOtherPurposeChecked(e.target.checked);
-  };
-  const handleTypeCheckBoxChange = (e) => {
-    setIsOtherTypeChecked(e.target.checked);
-  };
+    const triggerDatePicker = () => {
+        if(dateInputRef.current) {
+            dateInputRef.current.showPicker();
+        }
+    };
 
   return (
     <>
@@ -39,131 +31,68 @@ const CreateProjectComponent = () => {
                     <label className="form_label">
                       1. What is the name of your project?
                     </label>
-                    <input
-                      type="text"
-                      name="project_name"
-                      placeholder="e.g., My Awesome App"
-                      maxLength={10}
-                    />
+                    <input type="text" name="project_name" />
                     <label className="form_label">
                       2. What is the main purpose of the product?
                     </label>
                     <div className="radio_button_container">
-                      <div>
-                        <input
-                          type="checkbox"
-                          name="purpose"
-                          value="E-Commerce"
-                        />
-                        <span>E-Commerce</span>
-                      </div>
-                      <div>
-                        <input
-                          type="checkbox"
-                          name="purpose"
-                          value="Social Media"
-                        />
-                        <span>Social Media</span>
-                      </div>
-                      <div>
-                        <input
-                          type="checkbox"
-                          name="purpose"
-                          value="internal tool"
-                        />
-                        <span>internal tool</span>
-                      </div>
-
-                      <div>
-                        <input
-                          type="checkbox"
-                          name="purpose"
-                          onChange={handlePurposeCheckBoxChange}
-                          value="Other"
-                        />
-                        <span>Other</span>
-                      </div>
-                      {isOtherPurposeChecked && (
-                        <input
-                          type="text"
-                          name="other text"
-                          placeholder="Specify your purpose"
-                        />
-                      )}
+                      <input
+                        type="checkbox"
+                        name="purpose"
+                        value="E-Commerce"
+                      />
+                      <span>E-Commerce</span>
+                      <input
+                        type="checkbox"
+                        name="purpose"
+                        value="Social Media"
+                      />
+                      <span>Social Media</span>
+                      <input
+                        type="checkbox"
+                        name="purpose"
+                        value="internal tool"
+                      />
+                      <span>internal tool</span>
+                      <input type="checkbox" name="purpose" onChange="handleCheckBoxChange" value="Other" />
+                      <span>Other</span>
                     </div>
                     <label className="form_label">
                       3. What type of product do you want to build?
                     </label>
                     <div className="radio_button_container ">
-                      <div>
-                        <input type="checkbox" name="product" value="Website" />
-                        <span>Website</span>
-                      </div>
-                      <div>
+                      <input type="checkbox" name="product" value="Website" />
+                      <span>Website</span>
                       <input
                         type="checkbox"
                         name="product"
                         value="Android App"
                       />
                       <span>Android App</span>
-                      </div>
-                      <div>
                       <input type="checkbox" name="product" value="IOS App" />
                       <span>IOS App</span>
-                      </div>
-                      <div>
                       <input
                         type="checkbox"
                         name="product"
                         value="Windows Software"
                       />
                       <span>Windows Software</span>
-                      </div>
-                      <div>
-                      <input
-                        type="checkbox"
-                        name="product"
-                        onChange={handleTypeCheckBoxChange}
-                        value="Other"
-                      />
+                      <input type="checkbox" name="product" value="Other" />
                       <span>Other</span>
-                      </div>
-                      {isOtherTypeChecked && (
-                        <input
-                          type="text"
-                          name="other text"
-                          placeholder="Specify your product type"
-                        />
-                      )}
                     </div>
                     <label className="form_label">
-                      4. What is your estimated budget for this project (in
-                      CAD)?
+                      4. What is your estimated budget for this project?
                     </label>
-                    <input
-                      type="number"
-                      name="project_budget"
-                      placeholder="e.g., 2000"
-                      min="0"
-                    />
+                    <input type="text" name="project_budget" />
                     <label className="form_label">
-                      5. Please provide a brief description of your product:
+                      5. Who is the intended audience or users for your project?
                     </label>
-                    <input
-                      type="text"
-                      name="product_description"
-                      placeholder="e.g., A social media platform for connecting local communities"
-                      maxLength={250} // You can set a maximum length if needed
-                    />
+                    <input type="text" name="project_audience" />
                     <label className="form_label">
                       6. What are the main features or functionalities you want
                       to include in the project?
                     </label>
-                    <input
-                      type="text"
-                      name="project_deadline"
-                      placeholder="e.g., User login, real-time chat, payment integration"
-                    />
+                    <input type="text" name="project_deadline" />
                     <label className="form_label">
                       7. What is the expected timeline or deadline for the
                       project completion?
