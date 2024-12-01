07ec8285adf06fec5141c71ac8f4b95a84c5d12a
joshua-daniel <cherrydaniel11@gmail.com>
2024-12-01 10:43:39 -0500

    notification fix

---
+++ sparklink/src/notificationContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useContext } from "react";
+import axios from "axios";
+
+// Create the Context
+const NotificationContext = createContext();
+
+// Create a provider component
+export const NotificationProvider = ({ children }) => {
+  const [notifyCount, setNotifyCount] = useState(0);
+
+  // Function to update the notification count
+  const updateNotifyCount = async () => {
+    const response = await axios.get('/notify/count');
+      console.log("message ---> ",response.data.message);
+      console.log("count ---> ",response.data.notifCount);
+      setNotifyCount(response.data.notifCount);
+  };
+
+  return (
+    <NotificationContext.Provider value={{ notifyCount, updateNotifyCount }}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+// Custom hook to access notification count in any component
+export const useNotification = () => useContext(NotificationContext);
